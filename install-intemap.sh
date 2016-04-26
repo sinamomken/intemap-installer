8fdc54ac2071462e33092bdfc7f39adcbdc32f24
Sina Momken <sina.momken@gmail.com>
2016-04-26 18:13:21 +0430

    last version of InteMAP installer, before pausing work on it. It should completely work now.

--- install-intemap.sh
+++ install-intemap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# installation of base required utilites or skipping them if already installed:
+## Installation of base required utilites or skipping them if already installed:
 # bzip2
 # build-essentials including make, gcc, g++, etc.
 sudo apt-get --yes install bzip2 build-essential
@@ -11,6 +11,8 @@ if [ ! -d $INSTALL_PATH ]; then
 	sudo mkdir $INSTALL_PATH
 fi
 export INSTALL_PATH
+
+## Installation of dependency tools
 
 # install idba-ud
 ./install-idba-ud.sh
@@ -30,6 +32,28 @@ export INSTALL_PATH
 # install bowtie2
 ./install-bowtie2.sh
 
+## Compile and installation of InteMAP itself according to InteMAP_Manual.pdf
+# Extract and cd to InteMAP dir
+tar -xzvf InteMAP*.tar.gz
+cd InteMAP*
 
-# Including $INSTALL_PATH dirs in $PATH
+# Compile
+cd src/mergeassembly
+make
+cd ../../
+make
+chmod +x runInteMAP.py
+
+# Install (copy of files)
+if [ ! -d $INSTALL_PATH/intemap ]; then
+	sudo mkdir $INSTALL_PATH/intemap
+fi
+sudo cp -r * $INSTALL_PATH/intemap
+cd ..
+
+## Including $INSTALL_PATH bin dirs to $PATH
+ADD_TO_PATH="$INSTALL_PATH/abyss/bin/:$INSTALL_PATH/bowtie2/bin/:$INSTALL_PATH/idba/bin/:$INSTALL_PATH/intemap/:$INSTALL_PATH/jellyfish/bin/:$INSTALL_PATH/mummer/:$INSTALL_PATH/mummer/aux_bin/:$INSTALL_PATH/quake/bin/:$INSTALL_PATH/wgs/Linux-amd64/bin/"
+
+grep -q "PATH=\$PATH:$INSTALL_PATH/.*" /etc/profile && sudo sed -i "s|PATH=\$PATH:$INSTALL_PATH/.*|PATH=\$PATH:$ADD_TO_PATH|" /etc/profile  ||  echo "PATH=\$PATH:$ADD_TO_PATH" | sudo tee -a /etc/profile
+
 
